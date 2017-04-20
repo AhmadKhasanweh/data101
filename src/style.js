19f0ae6980f4a83a26c0430c0edbcbb6203e693c
AhmadKhasawneh <Ahmad.Khasawneh@hotmail.com>
2017-04-20 21:32:54 +0300

    Add style

---
+++ src/style.js
@@ -0,0 +1,21 @@
+const style = {
+  Mongobtn: {
+    marginTop: 100 + 'px', 
+    borderRadius: 25 + 'px'
+  },
+
+  MySQLbtn: {
+    marginTop: 100 + 'px', 
+    borderRadius: 25 + 'px'
+  }, 
+  leftCol: {
+    textAlign: 'right'
+  }, 
+  rightCol: {
+    textAlign: 'left'
+  },
+  midCol: {
+    textAlign: 'center'
+  }
+}
+module.exports = style;
